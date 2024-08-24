bcfabc7be5a8ed4dcf01a9d96063edff7ca34e03
Arjun Singh Naruka <singhnarukaarjun@gmail.com>
2024-08-24 17:46:20 +0530

    fvfvdf

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
-const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config({path: 'backend/config/config.env'});
-const connectDatabase = require('./config/database');
-const mysqlPool = require('./config/mysql_database');
-const PORT = process.env.PORT || 4000;
+const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config({ path: "backend/config/config.env" });
+const connectDatabase = require("./config/database");
+const mysqlPool = require("./config/mysql_database");
 
 //handling uncaught exceptions
-process.on('uncaughtException', (err) =>{
-    console.log('Error: '+err.message);
-    console.log('Shutting down the server due to unhandled uncaught exceptions');
-    process.exit(1);
-})
+process.on("uncaughtException", (err) => {
+  console.log("Error: " + err.message);
+  console.log("Shutting down the server due to unhandled uncaught exceptions");
+  process.exit(1);
+});
 
 //console.log(testing);
 
@@ -19,24 +18,25 @@ process.on('uncaughtException', (err) =>{
     console.log('mysql connection established');
 })*/
 
-mysqlPool.query('SELECT 1')
-    .then(([rows, fields]) => {
-        console.log('MySQL connection established');
-    })
-    .catch(err => {
-        console.error('Error establishing MySQL connection:', err);
-    });
-
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mysqlPool
+  .query("SELECT 1")
+  .then(([rows, fields]) => {
+    console.log("MySQL connection established");
+  })
+  .catch((err) => {
+    console.error("Error establishing MySQL connection:", err);
   });
 
-// unhandled promise rejection
-process.on('unhandledRejection', (err) => {
-    console.error('Error: ' + err.message);
-    console.error('Shutting down the server due to unhandled promise rejection');
+const server = app.listen(process.env.PORT, () => {
+  console.log("server is working on port " + process.env.PORT);
+});
 
-    server.close(() => {
-        process.exit(1);
-    });
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.error("Error: " + err.message);
+  console.error("Shutting down the server due to unhandled promise rejection");
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
